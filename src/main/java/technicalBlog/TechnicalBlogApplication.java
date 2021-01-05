40e1a195eb388ec65f27efc969cac0b43ce3ac42
Madhuri <madhukkl2000@gmail.com>
2021-01-05 14:38:10 +0530

    First milestone

---
+++ src/main/java/technicalBlog/TechnicalBlogApplication.java
@@ -0,0 +1,14 @@
+package technicalBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class TechnicalBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalBlogApplication.class, args);
+    }
+}
